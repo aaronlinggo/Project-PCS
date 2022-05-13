abbd47cc9f93405fe78dd4f8cac511651415f231
Samuel Gunawan <samgun9d32@gmail.com>
2022-05-13 16:34:49 +0700

    Test Commit 3

--- Hotel Harem SamGun/Hotel Harem SamGun/FormPemesananMakanan.Designer.cs
+++ Hotel Harem SamGun/Hotel Harem SamGun/FormPemesananMakanan.Designer.cs
@@ -29,13 +29,25 @@ namespace Hotel_Harem_SamGun
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 105);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FormPemesananMakanan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1685, 838);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormPemesananMakanan";
             this.Text = "FormPemesananMakanan";
             this.ResumeLayout(false);
@@ -43,5 +55,7 @@ namespace Hotel_Harem_SamGun
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
