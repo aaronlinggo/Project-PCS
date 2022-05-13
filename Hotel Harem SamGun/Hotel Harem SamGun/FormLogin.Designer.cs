8004cab5fb6c577b97e1d3a6aefc37bc6b8b6cf4
Samuel Gunawan <samgun9d32@gmail.com>
2022-05-13 16:22:34 +0700

    Test Commit 1

--- Hotel Harem SamGun/Hotel Harem SamGun/FormLogin.Designer.cs
+++ Hotel Harem SamGun/Hotel Harem SamGun/FormLogin.Designer.cs
@@ -29,15 +29,26 @@ namespace Hotel_Harem_SamGun
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(829, 553);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1685, 838);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "FormLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -47,6 +58,8 @@ namespace Hotel_Harem_SamGun
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
