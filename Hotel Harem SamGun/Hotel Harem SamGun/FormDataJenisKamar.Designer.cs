cabef79f106a89b2f5433bccc0f606a737a13859
Acxel Derian <acxelderian@gmail.com>
2022-05-16 00:14:55 +0700

    add functionality and UI FormDataJenisKamar

---
+++ Hotel Harem SamGun/Hotel Harem SamGun/FormDataJenisKamar.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace Hotel_Harem_SamGun
+{
+    partial class FormDataJenisKamar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBersihkan = new System.Windows.Forms.Button();
+            this.btnUbah = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rb0 = new System.Windows.Forms.RadioButton();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbHarga = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.dgvJenisKamar = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJenisKamar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBersihkan
+            // 
+            this.btnBersihkan.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBersihkan.Location = new System.Drawing.Point(165, 293);
+            this.btnBersihkan.Name = "btnBersihkan";
+            this.btnBersihkan.Size = new System.Drawing.Size(118, 43);
+            this.btnBersihkan.TabIndex = 14;
+            this.btnBersihkan.Text = "BERSIHKAN";
+            this.btnBersihkan.UseVisualStyleBackColor = true;
+            this.btnBersihkan.Click += new System.EventHandler(this.btnBersihkan_Click);
+            // 
+            // btnUbah
+            // 
+            this.btnUbah.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUbah.Location = new System.Drawing.Point(1140, 293);
+            this.btnUbah.Name = "btnUbah";
+            this.btnUbah.Size = new System.Drawing.Size(105, 43);
+            this.btnUbah.TabIndex = 13;
+            this.btnUbah.Text = "UBAH";
+            this.btnUbah.UseVisualStyleBackColor = true;
+            this.btnUbah.Click += new System.EventHandler(this.btnUbah_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbID);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.rb0);
+            this.groupBox1.Controls.Add(this.rb1);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.tbHarga);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbNama);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(26, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1219, 204);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // tbID
+            // 
+            this.tbID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbID.Location = new System.Drawing.Point(153, 19);
+            this.tbID.Name = "tbID";
+            this.tbID.ReadOnly = true;
+            this.tbID.Size = new System.Drawing.Size(126, 26);
+            this.tbID.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(24, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "ID";
+            // 
+            // rb0
+            // 
+            this.rb0.AutoSize = true;
+            this.rb0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rb0.Location = new System.Drawing.Point(270, 152);
+            this.rb0.Name = "rb0";
+            this.rb0.Size = new System.Drawing.Size(130, 24);
+            this.rb0.TabIndex = 15;
+            this.rb0.TabStop = true;
+            this.rb0.Text = "Tidak Tersedia";
+            this.rb0.UseVisualStyleBackColor = true;
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rb1.Location = new System.Drawing.Point(154, 152);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(88, 24);
+            this.rb1.TabIndex = 14;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Tersedia";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(24, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Status";
+            // 
+            // tbHarga
+            // 
+            this.tbHarga.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbHarga.Location = new System.Drawing.Point(153, 109);
+            this.tbHarga.Name = "tbHarga";
+            this.tbHarga.Size = new System.Drawing.Size(164, 26);
+            this.tbHarga.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Harga";
+            // 
+            // tbNama
+            // 
+            this.tbNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNama.Location = new System.Drawing.Point(153, 60);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(126, 26);
+            this.tbNama.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nama";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 31);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "DATA JENIS KAMAR";
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTambah.Location = new System.Drawing.Point(1018, 293);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(105, 43);
+            this.btnTambah.TabIndex = 10;
+            this.btnTambah.Text = "TAMBAH";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHapus.Location = new System.Drawing.Point(26, 293);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(118, 43);
+            this.btnHapus.TabIndex = 9;
+            this.btnHapus.Text = "HAPUS";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // dgvJenisKamar
+            // 
+            this.dgvJenisKamar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvJenisKamar.Location = new System.Drawing.Point(26, 356);
+            this.dgvJenisKamar.Name = "dgvJenisKamar";
+            this.dgvJenisKamar.ReadOnly = true;
+            this.dgvJenisKamar.RowHeadersVisible = false;
+            this.dgvJenisKamar.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvJenisKamar.Size = new System.Drawing.Size(1219, 308);
+            this.dgvJenisKamar.TabIndex = 8;
+            this.dgvJenisKamar.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvJenisKamar_CellClick);
+            // 
+            // FormDataJenisKamar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.btnBersihkan);
+            this.Controls.Add(this.btnUbah);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.dgvJenisKamar);
+            this.Name = "FormDataJenisKamar";
+            this.Text = "Data Jenis Kamar";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJenisKamar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBersihkan;
+        private System.Windows.Forms.Button btnUbah;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton rb0;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbHarga;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.DataGridView dgvJenisKamar;
+    }
+}
---
+++ Hotel Harem SamGun/Hotel Harem SamGun/FormDataJenisKamar.cs
@@ -0,0 +1,187 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel_Harem_SamGun
+{
+    public partial class FormDataJenisKamar : Form
+    {
+        MySqlConnection conn;
+        MySqlCommand cmd;
+        string query;
+        DataTable dt;
+        int selectedIdx = -1;
+        public FormDataJenisKamar()
+        {
+            Koneksi.openConn();
+            conn = Koneksi.conn;
+            InitializeComponent();
+            rb1.Checked = true;
+            resetTampilan();
+            refreshDGV();
+        }
+
+        public void refreshDGV()
+        {
+            dt = new DataTable();
+            query = "SELECT id_jenis_kamar,nama_jenis_kamar,harga_jenis_kamar,status_jenis_kamar FROM jenis_kamar ORDER BY id_jenis_kamar";
+            cmd = new MySqlCommand(query, conn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            dgvJenisKamar.DataSource = dt;
+            dgvJenisKamar.Columns[0].HeaderText = "ID Jenis Kamar";
+            dgvJenisKamar.Columns[1].HeaderText = "Nama Jenis Kamar";
+            dgvJenisKamar.Columns[2].HeaderText = "Harga Jenis Kamar";
+            dgvJenisKamar.Columns[3].HeaderText = "Status";
+            dgvJenisKamar.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dgvJenisKamar.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvJenisKamar.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dgvJenisKamar.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            selectedIdx = -1;
+        }
+
+        public void resetTampilan()
+        {
+            tbID.Text = "";
+            tbNama.Text = "";
+            tbHarga.Text = "";
+            rb1.Checked = true;
+            selectedIdx = -1;
+            generateID();
+        }
+
+        public void generateID()
+        {
+            query = "SELECT COUNT(*) FROM jenis_kamar";
+            cmd = new MySqlCommand(query, conn);
+            int jumlah = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+            jumlah++;
+            tbID.Text = jumlah.ToString();
+        }
+
+        private void dgvJenisKamar_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedIdx = dgvJenisKamar.CurrentCell.RowIndex;
+            tbID.Text = dgvJenisKamar.Rows[selectedIdx].Cells[0].Value.ToString();
+            tbNama.Text = dgvJenisKamar.Rows[selectedIdx].Cells[1].Value.ToString();
+            tbHarga.Text = dgvJenisKamar.Rows[selectedIdx].Cells[2].Value.ToString();
+            if (dgvJenisKamar.Rows[selectedIdx].Cells[3].Value.ToString() == "1")
+            {
+                rb1.Checked = true;
+            }
+            else
+            {
+                rb0.Checked = true;
+            }
+        }
+
+        private void btnBersihkan_Click(object sender, EventArgs e)
+        {
+            resetTampilan();
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            if (selectedIdx > -1)
+            {
+                int status;
+                if (rb1.Checked)
+                {
+                    status = 1;
+                }
+                else
+                {
+                    status = 0;
+                }
+                query = $"UPDATE jenis_kamar SET status_jenis_kamar = 0 WHERE id_jenis_kamar = '{tbID.Text}'";
+                cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Berhasil menghapus jenis kamar");
+                refreshDGV();
+                resetTampilan();
+            }
+            else
+            {
+                MessageBox.Show("Tidak bisa menghapus jenis kamar");
+            }
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            if (selectedIdx > -1)
+            {
+                //untuk update atau delete
+                MessageBox.Show("Tidak bisa insert di data yang sudah ada di database");
+            }
+            else
+            {
+                int harga;
+                if(!int.TryParse(tbHarga.Text, out harga))
+                {
+                    MessageBox.Show("Harga harus angka");
+                }
+                else if(harga < 0)
+                {
+                    MessageBox.Show("Harga minimalnya adalah 0");
+                }
+                else
+                {
+                    int status;
+                    if (rb1.Checked)
+                    {
+                        status = 1;
+                    }
+                    else
+                    {
+                        status = 0;
+                    }
+                    cmd = new MySqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = "INSERT INTO jenis_kamar (id_jenis_kamar, nama_jenis_kamar, harga_jenis_kamar, status_jenis_kamar) VALUES (@id, @nama, @harga, @status)";
+                    cmd.Parameters.Add(new MySqlParameter("@id", tbID.Text));
+                    cmd.Parameters.Add(new MySqlParameter("@nama", tbNama.Text));
+                    cmd.Parameters.Add(new MySqlParameter("@harga", harga));
+                    cmd.Parameters.Add(new MySqlParameter("@status", status));
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Berhasil menambah jenis kamar baru");
+                    refreshDGV();
+                    resetTampilan();
+                }
+                
+            }
+        }
+
+        private void btnUbah_Click(object sender, EventArgs e)
+        {
+            if (selectedIdx > -1)
+            {
+                int status;
+                if (rb1.Checked)
+                {
+                    status = 1;
+                }
+                else
+                {
+                    status = 0;
+                }
+                query = $"UPDATE jenis_kamar SET nama_jenis_kamar = '{tbNama.Text}', harga_jenis_kamar = '{tbHarga.Text}', status_jenis_kamar = '{status}' WHERE id_jenis_kamar = '{tbID.Text}'";
+                cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Berhasil mengubah jenis kamar");
+                refreshDGV();
+                resetTampilan();
+            }
+            else
+            {
+                MessageBox.Show("Tidak bisa mengubah jenis kamar");
+            }
+        }
+    }
+}
