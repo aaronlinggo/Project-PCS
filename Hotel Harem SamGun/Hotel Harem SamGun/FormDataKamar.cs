8527abbb4eb0ec193f9e4c4813c375b4fa826f7a
Acxel Derian <acxelderian@gmail.com>
2022-05-15 21:39:54 +0700

    Update Form Data Kamar

--- Hotel Harem SamGun/Hotel Harem SamGun/FormDataKamar.cs
+++ Hotel Harem SamGun/Hotel Harem SamGun/FormDataKamar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,19 +13,232 @@ namespace Hotel_Harem_SamGun
 {
     public partial class FormDataKamar : Form
     {
+        MySqlConnection conn;
+        MySqlCommand cmd;
+        string query;
+        DataTable dt;
+        int selectedIdx = -1;
+
         public FormDataKamar()
         {
             InitializeComponent();
+            rb1.Checked = true;
+            conn = new MySqlConnection();
+            Koneksi.openConn();
+            conn = Koneksi.conn;
+            refreshDGV();
+            isiComboKategori();
+            comboJenisKamar.SelectedIndex = 0;
+            tbNoKamar.Text = $"{numLantai.Value}" + ($"{numKamarKe.Value}").PadLeft(2, '0');
+            generateID();
+            generateKode();
         }
 
+
+        public void resetTampilan()
+        {
+            tbID.Text = "";
+            comboJenisKamar.SelectedIndex = 0;
+            numKamarKe.Value = 0;
+            numLantai.Value = 0;
+            rb1.Checked = true;
+            tbKode.Text = "";
+            tbNoKamar.Text = "";
+        }
+
+        public void refreshDGV()
+        {
+            dt = new DataTable();
+            query = "SELECT id_kamar, kode_kamar, nomor_kamar, nomor_lantai, nama_jenis_kamar, harga_jenis_kamar,status_kamar FROM kamar k JOIN jenis_kamar jk ON k.id_jenis_kamar = jk.id_jenis_kamar ORDER BY id_kamar";
+            cmd = new MySqlCommand(query, conn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            dgvKamar.DataSource = dt;
+            dgvKamar.Columns[0].HeaderText = "ID Kamar";
+            dgvKamar.Columns[1].HeaderText = "Kode Kamar";
+            dgvKamar.Columns[2].HeaderText = "Nomor Kamar";
+            dgvKamar.Columns[3].HeaderText = "Lantai";
+            dgvKamar.Columns[4].HeaderText = "Jenis Kamar";
+            dgvKamar.Columns[5].HeaderText = "Harga Kamar";
+            dgvKamar.Columns[6].HeaderText = "Status Kamar";
+            dgvKamar.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dgvKamar.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dgvKamar.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dgvKamar.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dgvKamar.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvKamar.Columns[5].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dgvKamar.Columns[6].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            selectedIdx = -1;
+        }
+        public void isiComboKategori()
+        {
+            query = "SELECT id_jenis_kamar, nama_jenis_kamar FROM jenis_kamar ORDER BY 1";
+            dt = new DataTable();
+            cmd = new MySqlCommand(query, conn);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            comboJenisKamar.DataSource = dt;
+            comboJenisKamar.DisplayMember = "nama_jenis_kamar";
+            comboJenisKamar.ValueMember = "id_jenis_kamar";
+        }
+
+        public void generateID()
+        {
+            query = "SELECT COUNT(*) FROM kamar";
+            cmd = new MySqlCommand(query, conn);
+            int jumlah = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+            jumlah++;
+            tbID.Text = jumlah.ToString();
+        }
+
+        public void generateKode()
+        {
+            query = $"SELECT CONCAT(UPPER(SUBSTR(nama_jenis_kamar,1,2)) , UPPER(SUBSTR(nama_jenis_kamar,INSTR(nama_jenis_kamar, ' ')+1,1))) FROM jenis_kamar WHERE id_jenis_kamar = '{comboJenisKamar.SelectedIndex+1}'";
+            cmd = new MySqlCommand(query, conn);
+            tbKode.Text = cmd.ExecuteScalar().ToString() + tbNoKamar.Text;
+        }
         private void numLantai_ValueChanged(object sender, EventArgs e)
         {
-            tbNama.Text = $"{numLantai.Value}" + ($"{numNoKamar.Value}").PadLeft(2,'0'); 
+            if (selectedIdx == -1)
+            {
+                generateID();
+            }
+            tbNoKamar.Text = $"{numLantai.Value}" + ($"{numKamarKe.Value}").PadLeft(2, '0');
+            generateKode();
         }
 
         private void numNoKamar_ValueChanged(object sender, EventArgs e)
         {
-            tbNama.Text = $"{numLantai.Value}" + ($"{numNoKamar.Value}").PadLeft(2, '0');
+            if(selectedIdx == -1)
+            {
+                generateID();
+            }
+            tbNoKamar.Text = $"{numLantai.Value}" + ($"{numKamarKe.Value}").PadLeft(2,'0');
+            generateKode();
+        }
+
+        private void dgvKamar_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedIdx = dgvKamar.CurrentCell.RowIndex;
+            tbID.Text = dgvKamar.Rows[selectedIdx].Cells[0].Value.ToString();
+            tbKode.Text = dgvKamar.Rows[selectedIdx].Cells[1].Value.ToString();
+            tbNoKamar.Text = dgvKamar.Rows[selectedIdx].Cells[2].Value.ToString();
+            numLantai.Value = Convert.ToInt32(dgvKamar.Rows[selectedIdx].Cells[3].Value.ToString());
+            numKamarKe.Value = Convert.ToInt32(dgvKamar.Rows[selectedIdx].Cells[2].Value.ToString().Substring(1,2));
+            cmd = new MySqlCommand($"SELECT id_jenis_kamar FROM jenis_kamar WHERE nama_jenis_kamar = '{dgvKamar.Rows[selectedIdx].Cells[4].Value.ToString()}'",conn);
+            int id_jenis_kamar = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+            comboJenisKamar.SelectedIndex = id_jenis_kamar-1;
+            if(dgvKamar.Rows[selectedIdx].Cells[6].Value.ToString() == "1")
+            {
+                rb1.Checked = true;
+            }
+            else
+            {
+                rb0.Checked = true;
+            }
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            if(selectedIdx > -1)
+            {
+                //update atau delete
+                MessageBox.Show("Tidak bisa insert di data yang sudah ada di database");
+            }
+            else
+            {
+                int nomor_kamar = Convert.ToInt32(tbNoKamar.Text);
+                query = $"SELECT COUNT(*) FROM kamar where nomor_kamar = '{nomor_kamar}'";
+                cmd = new MySqlCommand(query, conn);
+                int jumlah = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+                if (jumlah > 0)
+                {
+                    // ada nomor kamarnya
+                    MessageBox.Show("Nomor kamar sudah terdaftar");
+                }
+                else
+                {
+                    // belum ada, boleh insert
+                    int status;
+                    if (rb1.Checked)
+                    {
+                        status = 1;
+                    }
+                    else
+                    {
+                        status = 0;
+                    }
+                    cmd = new MySqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = "INSERT INTO kamar (id_kamar, kode_kamar, nomor_kamar, nomor_lantai, status_kamar, id_jenis_kamar) VALUES (@id, @kode, @nomor, @lantai, @status, @jenis)";
+                    cmd.Parameters.Add(new MySqlParameter("@id", tbID.Text));
+                    cmd.Parameters.Add(new MySqlParameter("@kode", tbKode.Text));
+                    cmd.Parameters.Add(new MySqlParameter("@nomor", tbNoKamar.Text));
+                    cmd.Parameters.Add(new MySqlParameter("@lantai", numLantai.Value));
+                    cmd.Parameters.Add(new MySqlParameter("@status", status));
+                    cmd.Parameters.Add(new MySqlParameter("@jenis", comboJenisKamar.SelectedValue));
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Berhasil menambah kamar baru");
+                    refreshDGV();
+                    resetTampilan();
+                }
+            }
+            
+        }
+
+        private void btnUbah_Click(object sender, EventArgs e)
+        {
+
+            if(selectedIdx > -1)
+            {
+                int status;
+                if(rb1.Checked)
+                {
+                    status = 1;
+                }
+                else
+                {
+                    status = 0;
+                }
+                query = $"UPDATE kamar SET kode_kamar = '{tbKode.Text}', nomor_kamar = '{tbNoKamar.Text}', nomor_lantai = '{numLantai.Value}',status_kamar = '{status}', id_jenis_kamar = '{comboJenisKamar.SelectedValue}' WHERE id_kamar = '{tbID.Text}'";
+                cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Berhasil mengubah kamar");
+                refreshDGV();
+                resetTampilan();
+            }
+            else
+            {
+                MessageBox.Show("Tidak bisa mengubah");
+            }
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            if (selectedIdx > -1)
+            {
+                query = $"UPDATE kamar SET status_kamar = '0' WHERE id_kamar = '{tbID.Text}'";
+                cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Berhasil menghapus kamar");
+                refreshDGV();
+                resetTampilan();
+            }
+            else
+            {
+                MessageBox.Show("Tidak bisa menghapus");
+            }
+        }
+
+        private void btnBersihkan_Click(object sender, EventArgs e)
+        {
+            resetTampilan();
+            refreshDGV();
+        }
+
+        private void comboJenisKamar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            generateKode();
         }
     }
 }
