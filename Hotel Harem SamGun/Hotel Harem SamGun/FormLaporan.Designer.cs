912740409340a87a9c2233c11ca5594d0e0c97d6
Acxel Derian <acxelderian@gmail.com>
2022-06-05 16:53:52 +0700

    Update UI FormLaporan

--- Hotel Harem SamGun/Hotel Harem SamGun/FormLaporan.Designer.cs
+++ Hotel Harem SamGun/Hotel Harem SamGun/FormLaporan.Designer.cs
@@ -29,21 +29,25 @@ namespace Hotel_Harem_SamGun
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLaporan));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnReport5 = new System.Windows.Forms.Button();
+            this.btnReport3 = new System.Windows.Forms.Button();
+            this.btnReport4 = new System.Windows.Forms.Button();
+            this.btnReport2 = new System.Windows.Forms.Button();
+            this.btnReport1 = new System.Windows.Forms.Button();
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.lblReportTitle = new System.Windows.Forms.Label();
-            this.btnReport1 = new System.Windows.Forms.Button();
-            this.btnReport2 = new System.Windows.Forms.Button();
-            this.btnReport4 = new System.Windows.Forms.Button();
-            this.btnReport3 = new System.Windows.Forms.Button();
-            this.btnReport5 = new System.Windows.Forms.Button();
-            this.btnBack = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.btnBack);
             this.panel1.Controls.Add(this.btnReport5);
             this.panel1.Controls.Add(this.btnReport3);
@@ -54,6 +58,90 @@ namespace Hotel_Harem_SamGun
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(268, 674);
             this.panel1.TabIndex = 0;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Gill Sans MT", 12F);
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.Location = new System.Drawing.Point(12, 591);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(242, 70);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "KEMBALI KE MENU UTAMA";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnReport5
+            // 
+            this.btnReport5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnReport5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReport5.Font = new System.Drawing.Font("Gill Sans MT", 12F);
+            this.btnReport5.ForeColor = System.Drawing.Color.White;
+            this.btnReport5.Location = new System.Drawing.Point(12, 447);
+            this.btnReport5.Name = "btnReport5";
+            this.btnReport5.Size = new System.Drawing.Size(242, 70);
+            this.btnReport5.TabIndex = 4;
+            this.btnReport5.Text = "LAPORAN PENJUALAN MAKANAN PALING LARIS";
+            this.btnReport5.UseVisualStyleBackColor = false;
+            this.btnReport5.Click += new System.EventHandler(this.btnReport5_Click);
+            // 
+            // btnReport3
+            // 
+            this.btnReport3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnReport3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReport3.Font = new System.Drawing.Font("Gill Sans MT", 12F);
+            this.btnReport3.ForeColor = System.Drawing.Color.White;
+            this.btnReport3.Location = new System.Drawing.Point(12, 295);
+            this.btnReport3.Name = "btnReport3";
+            this.btnReport3.Size = new System.Drawing.Size(242, 70);
+            this.btnReport3.TabIndex = 3;
+            this.btnReport3.Text = "LAPORAN JUMLAH TAMU PER BULAN";
+            this.btnReport3.UseVisualStyleBackColor = false;
+            this.btnReport3.Click += new System.EventHandler(this.btnReport3_Click);
+            // 
+            // btnReport4
+            // 
+            this.btnReport4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnReport4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReport4.Font = new System.Drawing.Font("Gill Sans MT", 12F);
+            this.btnReport4.ForeColor = System.Drawing.Color.White;
+            this.btnReport4.Location = new System.Drawing.Point(12, 371);
+            this.btnReport4.Name = "btnReport4";
+            this.btnReport4.Size = new System.Drawing.Size(242, 70);
+            this.btnReport4.TabIndex = 2;
+            this.btnReport4.Text = "LAPORAN JENIS KAMAR PALING LARIS";
+            this.btnReport4.UseVisualStyleBackColor = false;
+            this.btnReport4.Click += new System.EventHandler(this.btnReport4_Click);
+            // 
+            // btnReport2
+            // 
+            this.btnReport2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnReport2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReport2.Font = new System.Drawing.Font("Gill Sans MT", 12F);
+            this.btnReport2.ForeColor = System.Drawing.Color.White;
+            this.btnReport2.Location = new System.Drawing.Point(12, 219);
+            this.btnReport2.Name = "btnReport2";
+            this.btnReport2.Size = new System.Drawing.Size(242, 70);
+            this.btnReport2.TabIndex = 1;
+            this.btnReport2.Text = "LAPORAN PENAMBAHAN FASILITAS PALING LARIS";
+            this.btnReport2.UseVisualStyleBackColor = false;
+            this.btnReport2.Click += new System.EventHandler(this.btnReport2_Click);
+            // 
+            // btnReport1
+            // 
+            this.btnReport1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnReport1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReport1.Font = new System.Drawing.Font("Gill Sans MT", 12F);
+            this.btnReport1.ForeColor = System.Drawing.Color.White;
+            this.btnReport1.Location = new System.Drawing.Point(12, 143);
+            this.btnReport1.Name = "btnReport1";
+            this.btnReport1.Size = new System.Drawing.Size(242, 70);
+            this.btnReport1.TabIndex = 0;
+            this.btnReport1.Text = "LAPORAN PEMESANAN KAMAR BULANAN";
+            this.btnReport1.UseVisualStyleBackColor = false;
+            this.btnReport1.Click += new System.EventHandler(this.btnReport1_Click);
             // 
             // crystalReportViewer1
             // 
@@ -68,87 +156,40 @@ namespace Hotel_Harem_SamGun
             // lblReportTitle
             // 
             this.lblReportTitle.AutoSize = true;
-            this.lblReportTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReportTitle.Font = new System.Drawing.Font("Gill Sans MT", 20F, System.Drawing.FontStyle.Bold);
+            this.lblReportTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.lblReportTitle.Location = new System.Drawing.Point(274, 13);
             this.lblReportTitle.Name = "lblReportTitle";
-            this.lblReportTitle.Size = new System.Drawing.Size(231, 36);
+            this.lblReportTitle.Size = new System.Drawing.Size(229, 38);
             this.lblReportTitle.TabIndex = 2;
-            this.lblReportTitle.Text = "Laporan <???>";
+            this.lblReportTitle.Text = "LAPORAN <???>";
             // 
-            // btnReport1
+            // pictureBox1
             // 
-            this.btnReport1.Location = new System.Drawing.Point(12, 13);
-            this.btnReport1.Name = "btnReport1";
-            this.btnReport1.Size = new System.Drawing.Size(242, 70);
-            this.btnReport1.TabIndex = 0;
-            this.btnReport1.Text = "Laporan Pemesanan Kamar Bulanan";
-            this.btnReport1.UseVisualStyleBackColor = true;
-            this.btnReport1.Click += new System.EventHandler(this.btnReport1_Click);
-            // 
-            // btnReport2
-            // 
-            this.btnReport2.Location = new System.Drawing.Point(12, 89);
-            this.btnReport2.Name = "btnReport2";
-            this.btnReport2.Size = new System.Drawing.Size(242, 70);
-            this.btnReport2.TabIndex = 1;
-            this.btnReport2.Text = "Laporan Penambahan Fasilitas Paling Laris";
-            this.btnReport2.UseVisualStyleBackColor = true;
-            this.btnReport2.Click += new System.EventHandler(this.btnReport2_Click);
-            // 
-            // btnReport4
-            // 
-            this.btnReport4.Location = new System.Drawing.Point(12, 241);
-            this.btnReport4.Name = "btnReport4";
-            this.btnReport4.Size = new System.Drawing.Size(242, 70);
-            this.btnReport4.TabIndex = 2;
-            this.btnReport4.Text = "Laporan Tipe Kamar Paling Laris";
-            this.btnReport4.UseVisualStyleBackColor = true;
-            this.btnReport4.Click += new System.EventHandler(this.btnReport4_Click);
-            // 
-            // btnReport3
-            // 
-            this.btnReport3.Location = new System.Drawing.Point(12, 165);
-            this.btnReport3.Name = "btnReport3";
-            this.btnReport3.Size = new System.Drawing.Size(242, 70);
-            this.btnReport3.TabIndex = 3;
-            this.btnReport3.Text = "Laporan Jumlah Tamu per Bulan";
-            this.btnReport3.UseVisualStyleBackColor = true;
-            this.btnReport3.Click += new System.EventHandler(this.btnReport3_Click);
-            // 
-            // btnReport5
-            // 
-            this.btnReport5.Location = new System.Drawing.Point(12, 317);
-            this.btnReport5.Name = "btnReport5";
-            this.btnReport5.Size = new System.Drawing.Size(242, 70);
-            this.btnReport5.TabIndex = 4;
-            this.btnReport5.Text = "Laporan Penjualan Makanan Paling Laris";
-            this.btnReport5.UseVisualStyleBackColor = true;
-            this.btnReport5.Click += new System.EventHandler(this.btnReport5_Click);
-            // 
-            // btnBack
-            // 
-            this.btnBack.Location = new System.Drawing.Point(12, 591);
-            this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(242, 70);
-            this.btnBack.TabIndex = 5;
-            this.btnBack.Text = "Kembali ke Menu Utama";
-            this.btnBack.UseVisualStyleBackColor = true;
-            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(43, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 96);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
             // 
             // FormLaporan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1262, 673);
             this.Controls.Add(this.lblReportTitle);
             this.Controls.Add(this.crystalReportViewer1);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "FormLaporan";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FormLaporan";
+            this.Text = "Laporan";
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,5 +206,6 @@ namespace Hotel_Harem_SamGun
         private System.Windows.Forms.Label lblReportTitle;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btnReport5;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Hotel Harem SamGun/Hotel Harem SamGun/FormLaporan.cs
+++ Hotel Harem SamGun/Hotel Harem SamGun/FormLaporan.cs
@@ -19,42 +19,67 @@ namespace Hotel_Harem_SamGun
 
         private void btnReport1_Click(object sender, EventArgs e)
         {
-            lblReportTitle.Text = "Laporan Pemesanan Kamar Bulanan";
+            lblReportTitle.Text = "LAPORAN PEMESANAN KAMAR BULANAN";
             LaporanPemesananKamarBulanan mainReport = new LaporanPemesananKamarBulanan();
             mainReport.SetDatabaseLogon(Koneksi.uid, Koneksi.pass, Koneksi.server, Koneksi.database);
             crystalReportViewer1.ReportSource = mainReport;
+            btnReport1.BackColor = Color.FromArgb(255,69,0);
+            btnReport2.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport3.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport4.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport5.BackColor = Color.FromArgb(255, 128, 0);
         }
 
         private void btnReport2_Click(object sender, EventArgs e)
         {
-            lblReportTitle.Text = "Laporan Penambahan Fasilitas Paling Laris";
+            lblReportTitle.Text = "LAPORAN PENAMBAHAN FASILITAS PALING LARIS";
             LaporanPenambahanFasilitasPalingLaris mainReport = new LaporanPenambahanFasilitasPalingLaris();
             mainReport.SetDatabaseLogon(Koneksi.uid, Koneksi.pass, Koneksi.server, Koneksi.database);
             crystalReportViewer1.ReportSource = mainReport;
+            btnReport1.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport2.BackColor = Color.FromArgb(255, 69, 0);
+            btnReport3.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport4.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport5.BackColor = Color.FromArgb(255, 128, 0);
         }
 
         private void btnReport3_Click(object sender, EventArgs e)
         {
-            lblReportTitle.Text = "Laporan Jumlah Tamu per Bulan";
+            lblReportTitle.Text = "LAPORAN JUMLAH TAMU PER BULAN";
             LaporanJumlahTamuPerbulan mainReport = new LaporanJumlahTamuPerbulan();
             mainReport.SetDatabaseLogon(Koneksi.uid, Koneksi.pass, Koneksi.server, Koneksi.database);
             crystalReportViewer1.ReportSource = mainReport;
+            btnReport1.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport2.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport3.BackColor = Color.FromArgb(255, 69, 0);
+            btnReport4.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport5.BackColor = Color.FromArgb(255, 128, 0);
         }
 
         private void btnReport4_Click(object sender, EventArgs e)
         {
-            lblReportTitle.Text = "Laporan Tipe Kamar Paling Laris";
+            lblReportTitle.Text = "LAPORAN JENIS KAMAR PALING LARIS";
             LaporanPemesananTipeKamarPalingLaris mainReport = new LaporanPemesananTipeKamarPalingLaris();
             mainReport.SetDatabaseLogon(Koneksi.uid, Koneksi.pass, Koneksi.server, Koneksi.database);
             crystalReportViewer1.ReportSource = mainReport;
+            btnReport1.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport2.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport3.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport4.BackColor = Color.FromArgb(255, 69, 0);
+            btnReport5.BackColor = Color.FromArgb(255, 128, 0);
         }
 
         private void btnReport5_Click(object sender, EventArgs e)
         {
-            lblReportTitle.Text = "Laporan Penjualan Makanan Paling Laris";
+            lblReportTitle.Text = "LAPORAN PENJUALAN MAKANAN PALING LARIS";
             LaporanPenjualanMakananPalingLaku mainReport = new LaporanPenjualanMakananPalingLaku();
             mainReport.SetDatabaseLogon(Koneksi.uid, Koneksi.pass, Koneksi.server, Koneksi.database);
             crystalReportViewer1.ReportSource = mainReport;
+            btnReport1.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport2.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport3.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport4.BackColor = Color.FromArgb(255, 128, 0);
+            btnReport5.BackColor = Color.FromArgb(255, 69, 0);
         }
 
         private void btnBack_Click(object sender, EventArgs e)
